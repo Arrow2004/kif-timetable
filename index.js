4c10284610275dd4cae4576d8d16f3f93184873c
unknown <arrow2004hacker@gmail.com>
2022-09-15 15:42:18 +0300

    17:42 15.09

--- index.js
+++ index.js
@@ -109,9 +109,9 @@ bot.action('second', (ctx)=>{
             inline_keyboard: [
                 [
                     { text: '210-21 KIo\'', callback_data: 'kio210_21'},
+                    { text: '211-21 KIo\'', callback_data: 'kio211_21'},
                 ],
                 [
-                    { text: '211-21 KIo\'', callback_data: 'kio211_21'},
                     { text: '212-21 KIo\'', callback_data: 'kio212_21'},
                     { text: '213-21 KIo\'', callback_data: 'kio213_21'},
                     { text: '214-21 KIo\'', callback_data: 'kio214_21'}
@@ -120,16 +120,19 @@ bot.action('second', (ctx)=>{
                     { text: '215-21 ATo\'', callback_data: 'ato215_21'},
                     { text: '216-21 ATo\'', callback_data: 'ato216_21'},
                     { text: '217-21 ATo\'', callback_data: 'ato217_21'},
-                    { text: '218-21 ATo\'', callback_data: 'ato218_21'}
+
                 ],
                 [
+                    { text: '218-21 ATo\'', callback_data: 'ato218_21'},
                     { text: '219-21 MTo\'', callback_data: 'mto219_21'},
                     { text: '220-21 MTo\'', callback_data: 'mto220_21'},
+                ],
+                [   
                     { text: '221-21 MRo\'', callback_data: 'mro221_21'},
                     { text: '222-21 MRo\'', callback_data: 'mro222_21'},
+                    { text: '223-21 SIo\'', callback_data: 'sio223_21'},
                 ],
                 [
-                    { text: '223-21 SIo\'', callback_data: 'sio223_21'},
                     { text: '224-21 SIo\'', callback_data: 'sio224_21'},
                     { text: '230-21 KIr', callback_data: 'kir230_21'},
                     { text: '231-21 KIr\'', callback_data: 'kir231_21'},
@@ -138,13 +141,15 @@ bot.action('second', (ctx)=>{
                     { text: '232-21 ATr', callback_data: 'atr232_21'},
                     { text: '233-21 ATr\'', callback_data: 'atr233_21'},
                     { text: '234-21 MTr', callback_data: 'mtr234_21'},
-                    { text: '235-21 MRr\'', callback_data: 'mrr235_21'},
                 ],
                 [
+                  { text: '235-21 MRr\'', callback_data: 'mrr235_21'},
                     { text: '236-21 Sir', callback_data: 'sir236_21'},
-                    { text: '240-21 KIo\'', callback_data: 'kio240_21'},
-                    { text: '241-21 KIo\'', callback_data: 'kio241_21'},
                 ],
+                [
+                  { text: '240-21 KIo\'', callback_data: 'kio240_21'},
+                  { text: '241-21 KIo\'', callback_data: 'kio241_21'},
+                ]
             ]
         }
     })
@@ -155,15 +160,15 @@ bot.action('third', (ctx)=>{
             inline_keyboard: [
                 [
                     { text: '210-20 KIo\'', callback_data: 'kio210_20'},
-                ],
-                [
                     { text: '211-20 KIo\'', callback_data: 'kio211_20'},
                     { text: '212-20 KIo\'', callback_data: 'kio212_20'},
-                    { text: '213-20 KIo\'', callback_data: 'kio213_20'},
-                    { text: '214-20 Kling', callback_data: 'kling214_20'}
                 ],
                 [
+                    { text: '213-20 KIo\'', callback_data: 'kio213_20'},
+                    { text: '214-20 Kling', callback_data: 'kling214_20'},
                     { text: '215-20 ATo\'', callback_data: 'ato215_20'},
+                ],
+                [
                     { text: '216-20 ATo\'', callback_data: 'ato216_20'},
                     { text: '217-20 MTo\'', callback_data: 'mto217_20'},
                     { text: '218-20 MTo\'', callback_data: 'mto218_20'}
@@ -172,8 +177,10 @@ bot.action('third', (ctx)=>{
                     { text: '220-20 KIr', callback_data: 'kir220_20'},
                     { text: '221-20 KIr', callback_data: 'kir221_20'},
                     { text: '222-20 ATr', callback_data: 'atr222_20'},
-                    { text: '223-20 MTr\'', callback_data: 'mtr223_20'}
                 ],
+                [
+                  { text: '223-20 MTr\'', callback_data: 'mtr223_20'}
+                ]
             ]
         }
     })
@@ -184,15 +191,15 @@ bot.action('fourth', (ctx)=>{
             inline_keyboard: [
                 [
                     { text: '210-19 KIo\'', callback_data: 'kio210_19'},
-                ],
-                [
                     { text: '211-19 KIo\'', callback_data: 'kio211_19'},
                     { text: '212-19 KIo\'', callback_data: 'kio212_19'},
+                ],
+                [    
                     { text: '213-19 KIo\'', callback_data: 'kio213_19'},
-                    { text: '214-19 Kling', callback_data: 'kling214_19'}
+                    { text: '214-19 Kling', callback_data: 'kling214_19'},
+                    { text: '215-19 ATo\'', callback_data: 'ato215_19'},
                 ],
                 [
-                    { text: '215-19 ATo\'', callback_data: 'ato215_19'},
                     { text: '216-19 ATo\'', callback_data: 'ato216_19'},
                     { text: '217-19 MTo\'', callback_data: 'mto217_19'},
                     { text: '218-19 MTo\'', callback_data: 'mto218_19'}
@@ -201,8 +208,10 @@ bot.action('fourth', (ctx)=>{
                     { text: '220-19 KIr', callback_data: 'kir220_19'},
                     { text: '221-19 KIr', callback_data: 'kir221_19'},
                     { text: '222-19 ATr', callback_data: 'atr222_19'},
-                    { text: '223-19 MTr\'', callback_data: 'mtr223_19'}
                 ],
+                [
+                  { text: '223-19 MTr\'', callback_data: 'mtr223_19'}
+                ]
             ]
         }
     })
